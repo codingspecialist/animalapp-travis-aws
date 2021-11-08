35eb028042998b039cf7c44430026f7ad10916ef
ssar <ssar@nate.com>
2021-11-08 16:51:59 +0900

    폴더 생성 로직 추가 재 배포

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,9 @@ REPOSITORY=/home/ubuntu/webapp
 DEPLOY_DIRECTORY=/home/ubuntu/deploy
 PROJECT_NAME=animalapp
 
-echo "2. jar 파일 webapp 폴더로 복사하기"
+echo "2. jar 파일 webapp/animalapp 폴더로 복사하기"
 
+mkdir $REPOSITORY/$PROJECT_NAME
 cp $DEPLOY_DIRECTORY/build/libs/*.jar $REPOSITORY/$PROJECT_NAME/animalapp.jar
 
 # 현재 구동중인 애플리케이션 pid 확인해서 강제종료 시키고 새 애플리케이션 실행 필요!! - Profile 모드 적용!!
