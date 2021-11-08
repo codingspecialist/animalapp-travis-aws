333a07e2d22bc501da9f7919f44b3344edb61f75
ssar <ssar@nate.com>
2021-11-08 17:57:54 +0900

    스크립트 파일 수정 - 강제 삭제 폴더 rf 옵션 주기

--- deploy.sh
+++ deploy.sh
@@ -22,8 +22,8 @@ fi
 
 echo "3. jar 파일 webapp/animalapp 폴더로 복사하기"
 
-sudo rm -r $REPOSITORY/$PROJECT_NAME
-sudo mkdir $REPOSITORY/$PROJECT_NAME
+rm -rf $REPOSITORY/$PROJECT_NAME
+mkdir $REPOSITORY/$PROJECT_NAME
 cp $DEPLOY_DIRECTORY/build/libs/*.jar $REPOSITORY/$PROJECT_NAME/animalapp.jar
 
 echo "4. jar파일 실행권한 주고 nohup 으로 실행하기 - 출력 재지정(Redirect) 안하면 오류남!!"
