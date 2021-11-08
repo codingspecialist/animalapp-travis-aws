0e4639f5f5ddf8e03991e681c4dd189e1b0841e6
ssar <ssar@nate.com>
2021-11-08 14:18:30 +0900

    배포 테스트

--- src/main/java/com/cos/animalapp/AnimalController.java
+++ src/main/java/com/cos/animalapp/AnimalController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AnimalController {
-
+  
 	@GetMapping("/")
-	public String home() {
+	public String home2() {
 		return "home";
 	}
 	
