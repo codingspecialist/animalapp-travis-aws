1c0d1b0ffac53c18b67e748163469d6317f20911
ssar <ssar@nate.com>
2021-11-08 17:46:12 +0900

    서버 종료(버그해결) 후 폴더 삭제세팅완료

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,11 @@ DEPLOY_DIRECTORY=/home/ubuntu/deploy
 PROJECT_NAME=animalapp
 JAR_NAME=$REPOSITORY/$PROJECT_NAME/animalapp.jar
 
-echo "2. jar 파일 webapp/animalapp 폴더로 복사하기"
+echo "2. 현재 구동중인 애플리케이션 pid 확인해서 강제종료하기"
 
-mkdir $REPOSITORY/$PROJECT_NAME
-cp $DEPLOY_DIRECTORY/build/libs/*.jar $REPOSITORY/$PROJECT_NAME/animalapp.jar
+CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep java | awk '{print $1}')
 
-echo "3. 현재 구동중인 애플리케이션 pid 확인해서 강제종료하기"
-
-CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{print $1}')
+echo "$CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
    echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
@@ -22,6 +19,12 @@ else
    kill -15 $CURRENT_PID
    sleep 5
 fi
+
+echo "3. jar 파일 webapp/animalapp 폴더로 복사하기"
+
+sudo rm -r $REPOSITORY/$PROJECT_NAME
+sudo mkdir $REPOSITORY/$PROJECT_NAME
+cp $DEPLOY_DIRECTORY/build/libs/*.jar $REPOSITORY/$PROJECT_NAME/animalapp.jar
 
 echo "4. jar파일 실행권한 주고 nohup 으로 실행하기 - 출력 재지정(Redirect) 안하면 오류남!!"
 
